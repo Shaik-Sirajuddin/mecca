c4400f3503582b507073b8693f2b91b2514daf20
Shaik-Sirajuddin <sirajuddinshaik30@gmail.com>
2025-01-02 17:02:02 +0000

    feat: update build config

--- staking/dapp/vite.config.ts
+++ staking/dapp/vite.config.ts
@@ -1,24 +1,41 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-
+import inject from "@rollup/plugin-inject";
 import { NodeGlobalsPolyfillPlugin } from "@esbuild-plugins/node-globals-polyfill";
-
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  optimizeDeps: {
-    esbuildOptions: {
-      define: {
-        global: "globalThis",
-      },
-      plugins: [
-        NodeGlobalsPolyfillPlugin({
-          buffer: true,
-        }),
-      ],
+export default defineConfig(({ mode }) => {
+  return {
+    plugins: [react()],
+    build:
+      mode === "production"
+        ? {
+            rollupOptions: {
+              plugins: [inject({ Buffer: ["buffer", "Buffer"] })],
+            },
+          }
+        : undefined,
+    define:
+      mode === "development"
+        ? {
+            global: {},
+          }
+        : undefined,
+
+    optimizeDeps: {
+      esbuildOptions:
+        mode === "development"
+          ? {
+              define: {
+                global: "globalThis",
+              },
+              plugins: [
+                NodeGlobalsPolyfillPlugin({
+                  buffer: true,
+                  process: true,
+                }),
+              ],
+            }
+          : undefined,
     },
-  },
-  define: {
-    global: "globalThis",
-  },
+  };
 });
