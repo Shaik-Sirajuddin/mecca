ad1e80df2482e9dd983709df48c0211f9088c640
Shaik-Sirajuddin <sirajuddinshaik30@gmail.com>
2025-01-03 12:38:33 +0530

    feat: update constants

--- airdrop/backend/src/constants.ts
+++ airdrop/backend/src/constants.ts
@@ -1,11 +1,11 @@
 import { PublicKey } from "@solana/web3.js";
 
 export const tokenMintId = new PublicKey(
-  "tkdZ2grPbhAcZ9W1gXaWhoNf6rJdCgqs9St7DFxdy7A"
+  "mecySk7eSawDNfAXvW3CquhLyxyKaXExFXgUUbEZE1T"
 );
 //owner : ahSUozSv5vWRrX79wqGrfQzJwugrbKdMu6PBgEkLhnT
 export const tokenHolderAtaId = new PublicKey(
-  "FkZKW4cTZvdW7EcDbmsNinLeLhhdBvDCNwf3SA7vHbnK"
+  "4TmET6N7oCxom6DLc3rBPZQgviG3q86SsrFodYaBnTq9"
 );
 
 export const tokenProgramId = new PublicKey(
