bed71dc5743d81199cfce7f347d0cf2e0422cbe7
Shaik-Sirajuddin <sirajuddinshaik30@gmail.com>
2025-01-29 10:56:35 +0530

    feat: add stats route

---
+++ game/backend/api/admin/stats.go
@@ -0,0 +1,58 @@
+package admin
+
+import (
+	"beluga/api/utils"
+	"beluga/db"
+	"beluga/models"
+	"beluga/session"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"github.com/shopspring/decimal"
+)
+
+func Stats(c *gin.Context) {
+	dau := session.GetTodayLoginCount(c.Request.Context())
+
+	var userCount int64
+
+	if result := db.DB.Model(&models.User{}).Count(&userCount).Error; result != nil {
+		println(result.Error())
+		utils.ResIntenalError(c)
+		return
+	}
+
+	type HodingsValue struct {
+		Value decimal.Decimal `json:"total_holdings_value"`
+		Coins decimal.Decimal `json:"total_coins"`
+	}
+
+	holdingsValue := HodingsValue{}
+
+	if err := db.DB.Raw(
+		`SELECT SUM(bh.value * bv.quantity) AS total_holdings_value
+				FROM beluga_holdings bh
+				JOIN beluga_variants bv ON bh.id = bv.beluga_id;
+       `, &holdingsValue.Value).Error; err != nil {
+		utils.ResIntenalError(c)
+		return
+	}
+
+	if err := db.DB.Raw(
+		`SELECT SUM(coins) AS total_coins
+				FROM users;
+       `, &holdingsValue.Coins).Error; err != nil {
+		utils.ResIntenalError(c)
+		return
+	}
+
+	c.IndentedJSON(http.StatusOK, gin.H{
+		"message": "Data Fetched",
+		"data": gin.H{
+			"dau":           dau,
+			"totalUsers":    userCount,
+			"holdingsValue": holdingsValue.Value.String(),
+			"totalCoins":    holdingsValue.Coins.String(),
+		},
+	})
+}
