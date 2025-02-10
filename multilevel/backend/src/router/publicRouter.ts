cbe98361b34f089fb268ada57e7a382af1c4e6a6
Shaik-Sirajuddin <sirajuddinshaik30@gmail.com>
2025-02-10 23:20:21 +0530

    feat: add search user function

--- multilevel/backend/src/router/publicRouter.ts
+++ multilevel/backend/src/router/publicRouter.ts
@@ -4,8 +4,11 @@ const publicRouter = Router();
 
 publicRouter.post("/join", publicController.join);
 publicRouter.post("/referrer-chart", publicController.getReferrerChartData);
+
 publicRouter.get("/user-stats", publicController.getUserStats);
 publicRouter.get("/users", publicController.getUsers);
+publicRouter.get("/search-user", publicController.searchUser);
+
 publicRouter.get("/code", publicController.getUniqueCodeForUser);
 publicRouter.post("/address-from-code", publicController.getAddressFromCode);
 publicRouter.post("/crew", publicController.getCrewList);
