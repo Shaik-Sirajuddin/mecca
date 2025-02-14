a0aaf7103d6c75aa661d636ec13b17f73a1f2f43
Shaik-Sirajuddin <sirajuddinshaik30@gmail.com>
2025-02-14 10:39:14 +0530

    feat: otp login feature admin

--- airdrop/backend/src/app.ts
+++ airdrop/backend/src/app.ts
@@ -2,6 +2,7 @@ import * as dotenv from "dotenv";
 import { intializeDB } from "./database/initData";
 import server from "./config/server";
 import { makeConnection } from "./database/connection";
+import * as speakeasy from "speakeasy";
 
 dotenv.config();
 const port = 3020;
@@ -13,3 +14,12 @@ server.listen(port, () => {
 makeConnection().then((res) => {
   intializeDB();
 });
+
+function generate_secret_key() {
+  // Generate a secret key
+  const secretKey = speakeasy.generateSecret({ length: 20 });
+
+  return secretKey;
+}
+
+console.log(generate_secret_key())
--- airdrop/backend/src/router/adminRouter.ts
+++ airdrop/backend/src/router/adminRouter.ts
@@ -1,11 +1,26 @@
 import { Router } from "express";
 import * as adminController from "../controller/adminController";
+import { adminMiddleware } from "../middleware/adminMiddleware";
 
 const adminRouter = Router();
 
-adminRouter.post("/update-config", adminController.updateConfig);
-adminRouter.get("/login", adminController.login);
-adminRouter.post("/claims", adminController.claimsByDate);
-adminRouter.get("/total-claims", adminController.getTotalClaims);
+adminRouter.post(
+  "/update-config",
+  adminMiddleware,
+  adminController.updateConfig
+);
+adminRouter.post("/login", adminController.login);
+adminRouter.get("/logout", adminController.logout);
+adminRouter.post("/claims", adminMiddleware, adminController.claimsByDate);
+adminRouter.get(
+  "/verify-login",
+  adminMiddleware,
+  adminController.validateLogin
+);
+adminRouter.get(
+  "/total-claims",
+  adminMiddleware,
+  adminController.getTotalClaims
+);
 
 export default adminRouter;
